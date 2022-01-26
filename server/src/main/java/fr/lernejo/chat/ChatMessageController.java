890efdf476b96d94ccd727a44e0ec61de74a3ea3
lounes.bounouar <lounes.bounouar@epita.fr>
2022-01-26 12:46:18 +0100

    Part 5 finished and compiling

--- server/src/main/java/fr/lernejo/chat/ChatMessageController.java
+++ server/src/main/java/fr/lernejo/chat/ChatMessageController.java
@@ -1,6 +1,5 @@
 package fr.lernejo.chat;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- server/src/main/java/fr/lernejo/chat/ChatMessageListener.java
+++ server/src/main/java/fr/lernejo/chat/ChatMessageListener.java
@@ -1,6 +1,5 @@
 package fr.lernejo.chat;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
